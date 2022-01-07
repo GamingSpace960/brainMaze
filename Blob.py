29fb7975436919d7b3c444fc398e1a8a4523175d
logan caffey <spikedhair999@gmail.com>
2022-01-06 20:04:23 -0600

    finished leader bots 1.0

--- Blob.py
+++ Blob.py
@@ -1,12 +1,78 @@
 import numpy as np
 import pygame as pg
-import World.py
+import World
+from time import sleep
 
-class Blob():
-    def __init__(self,count,map):
-        pg.draw.circle(map,(10,10,250),(400,400),2)
+class LeadBlob():
+    def __init__(self,world):
+        #bounds (370,310)-(450,370)
+        # max spots = (450,370)/2 = (225,185)
+        self.world = world
+        self.restart()
+        self.speed = np.random.randint(1,5)
+        self.mindSizeX = np.random.randint(5,250+1)
+        self.mindSizeY = np.random.randint(5,185+1)
+        self.leadership = np.random.randint(1,50+1)
+        self.qTable()
+
+    def qTable(self):
+        self.qtable = np.random.randn(self.mindSizeX,self.mindSizeY,4)
+
+    def Action(self,epsilon):
+        if self.dead:
+            return
+        if 250-(self.me.x/2) < 0 or 185-(self.me.y/2) < 0:
+            print(250-(self.me.x/2),185-(self.me.y/2),self.me.x,self.me.y)
+        if np.random.random() > epsilon and len(self.qtable) > 250-(self.me.x/2) and len(self.qtable[int(250-(self.me.x/2))]) > 185-(self.me.y/2):
+            self.action = np.argmax(self.qtable[int(250-(self.me.x/2))][int(185-(self.me.y/2))])
+        else:
+            self.action = np.random.randint(4)
+        if self.action == 0:
+            self.me.move_ip(-2*self.speed,0)
+        elif self.action == 1:
+            self.me.move_ip(2*self.speed,0)
+        elif self.action == 2:
+            self.me.move_ip(0,-2*self.speed)
+        else:
+            self.me.move_ip(0,2*self.speed)
+        if self.me.collidelist(self.world.walls) != -1:
+            self.dead = True
+            self.score -= 1000
+            return
+        elif self.me.colliderect(self.world.goal):
+            self.score += 100
+        elif (self.me.x,self.me.y) in self.beenList:
+            self.score -= 10
+        else:
+            self.score -=.1
+        pg.draw.rect(self.world.win,(10,10,200),self.me)
+    def restart(self):
+        x = np.random.randint(370/2,450/2+.5)*2
+        y = np.random.randint(310/2,370/2+.5)*2
+        self.me = pg.draw.rect(self.world.win,(10,10,200),(x,y,2,2))
+        self.beenList = []
+        self.dead = False
+        self.score = 0
 
 
         
-map = World.World()
-bob = Blob(1,map)
+world = World.World()
+bobs = [LeadBlob(world) for _ in range(10_000)]
+
+epsilon = 1
+epsilonDecay = .001
+step = 0
+while True:
+    while step < 10_000 and False in [bob.dead for bob in bobs]:
+        world.draw()
+        for bob in bobs:
+            bob.Action(epsilon)
+        world.update()
+        step+= 1
+        
+    for bob in bobs:
+            bob.restart()
+    epsilon -= epsilon*epsilonDecay
+    print(epsilon)
+    step = 0
+    done = False
--- World.py
+++ World.py
@@ -1,12 +1,13 @@
 import pygame as pg
+import numpy as np
 
 class World():
     def __init__(self):
         pg.init()
-        win = pg.display.set_mode(size = (500,500))
+        self.win = pg.display.set_mode(size = (500,500))
         #backGround = pg.image.load('C:/Users/spike/Pictures/Untitled_Artwork.jpg')
         #win.blit(backGround, (0,0))
-        wallsPos = [(0,0,500,10),(0,10,10,270),(490,10,10,270),(450,270,40,10),(30,270,340,10),(360,280,10,100),(450,280,10,100),(370,370,80,10),
+        self.wallsPos = [(0,0,500,10),(0,10,10,270),(490,10,10,270),(450,270,40,10),(30,270,340,10),(360,280,10,100),(450,280,10,100),(370,370,80,10),
                     (10,30,250,10),(280,30,100,10),(400,30,40,10),(310,70,60,10),(10,80,60,10),(130,80,50,10),(210,80,20,10),
                     (260,100,20,10),(310,100,40,10),(10,120,70,10),(140,130,230,10),(100,160,190,10),(370,180,50,10),
                     (10,200,200,10),(280,200,60,10),(190,210,70,10),(280,230,40,10),(370,230,80,10),
@@ -14,10 +15,17 @@ class World():
                     (70,130,10,50),(100,130,10,30),(340,140,10,130),(30,150,10,50),(450,150,10,90),(130,170,10,30),(230,170,10,20),(370,190,10,40),(60,210,30,40),
                     (160,210,10,40),(250,210,10,30),(280,210,10,20),(400,210,10,100),(30,230,10,40),(110,230,30,40),(190,240,10,30),(220,250,10,20)]
         # exterior walls then by row then by column
-        walls = [pg.draw.rect(win,(200,10,10),wall) for wall in wallsPos]
+        self.draw()
+   
+    def draw(self):
+        self.back = pg.draw.rect(self.win,(10,10,10),(0,0,500,500))
+        self.walls = [pg.draw.rect(self.win,(200,10,10),wall) for wall in self.wallsPos]
+        self.goal = pg.draw.rect(self.win,(10,200,10),(10,270,20,10))
 
-        while True:
-            pg.display.flip()
+    def update(self):
+        pg.display.update()
+        pg.event.pump()
+
         
 
-map = World() 
+map = World()
